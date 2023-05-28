31f6cd9b1e71ee4ef92a1fa1fcb08b9f6930d7fb
unknown <mungekar.kiran30@gmail.com>
2023-05-28 17:57:02 +0530

    Post view component added

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,14 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUsersForClient = (user: User) => {
+  return {
+    id: user.id, username: user.username, profilePicture: user.profileImageUrl,
+  }
+}
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -9,7 +18,33 @@ export const postRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+     take: 100,
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map(post => post.authorId),
+      limit: 100,
+    })).map(filterUsersForClient);
+
+    return posts.map((post) =>{
+      const author = users.find((user) => user.id === post.authorId)
+      if(!author || !author.username){
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found!"
+        })
+      }
+      return {
+      post,
+      author: {
+        ...author,
+        username: author.username
+      },
+      }
+    });
   }),
+
+
 });
